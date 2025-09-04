b512eeef9cb9506fcc6ca754c3171a73ba0e86b4
fahad936 <bukolajimoh936@gmail.com>
2025-09-04 14:35:44 +0100

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+console.log(5 + 7)
+console.log(15 - 7)
+console.log(35 / 7)
+console.log(5 * 7)
+console.log(5 ** 4)
+console.log(5 % 7)
+
+//
+
+let total = 0
+let price =30
+let discount =2.5
+total += price - discount
